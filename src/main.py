6a3a56f0b29f398702be9522c0aaa28b2bb7d736
luweihai <644475880@qq.com>
2019-04-03 14:03:49 +0800

    整理  以后其他的比赛可以套用这个模板

--- src/main.py
+++ src/main.py
@@ -17,11 +17,11 @@ def get_data(data,feature,shape):
 
 
 x_train_1,x_test_1 = get_data(data,feature1,50000)
-x_train_2,x_test_2 = get_data(data,feature2,50000)
-x_train_3,x_test_3 = get_data(data,feature3,50000)
-x_train_4,x_test_4 = get_data(data,feature4,50000)
-x_train_5,x_test_5 = get_data(data,feature5,50000)
-x_train_6,x_test_6 = get_data(data,feature6,50000)
+x_train_2,x_test_2 = get_data(data,feature1,50000)
+x_train_3,x_test_3 = get_data(data,feature2,50000)
+x_train_4,x_test_4 = get_data(data,feature2,50000)
+x_train_5,x_test_5 = get_data(data,feature3,50000)
+x_train_6,x_test_6 = get_data(data,feature3,50000)
 
 
 lgb1_model(1,x_train_1,y_train,x_test_1,'1')
